3bfba1b40bfeffb33c456c623c6421f268cf5b86
nitayke <nitayke@gmail.com>
2021-08-08 12:31:07 +0300

    Added a few comments in connection-worker class

--- include/M-R-VulkaN/ConnectionWorker.h
+++ include/M-R-VulkaN/ConnectionWorker.h
@@ -10,12 +10,14 @@
 #include <functional>
 #include <atomic>
 
-typedef void (*PacketCallbackFunc)(bitcraze::crazyflieLinkCpp::Packet);
+
 
 //The PacketCallback returns a boolean,
 //If it returns true then the callback will be called for another loop
 //If it returns false then the callback is removed from the list
 typedef std::function<bool(bitcraze::crazyflieLinkCpp::Packet)> PacketCallback;
+
+//Includes the port and channel of the callback
 struct PacketCallbackBundle
 {
     uint8_t _port;
@@ -23,17 +25,20 @@ struct PacketCallbackBundle
     PacketCallback _packetCallbackFunc;
 };
 
+//This class is the main worker class that is responsible for sending callbacks
+// on received packets from the crazyflie server
+//It is also a thin rapper around the Connection class 
 class ConnectionWorker
 {
 private:
-    std::list<PacketCallbackBundle> _paramReceivedCallbacks;
-    std::thread _receivingThread;
-    std::mutex _threadSleepMutex;
-    std::mutex _callbackMutex;
+    std::list<PacketCallbackBundle> _paramReceivedCallbacks;//The list of callbacks that are present
+    std::thread _receivingThread;//The mutex for receiving packets
+    std::mutex _threadSleepMutex; //The mutex for changing the receive-thread's state from active to inactive or vice versa
+    std::mutex _callbackMutex; // The mutex for running a callback function
 
-    std::condition_variable _threadSleepConVar;
-    std::atomic<bitcraze::crazyflieLinkCpp::Connection *> _conAtomicPtr;
-    std::atomic<bool> _deactivateThread;
+    std::condition_variable _threadSleepConVar; //Condition variable which is used for waiting whenever the receive-thread is sleeping
+    std::atomic<bitcraze::crazyflieLinkCpp::Connection *> _conAtomicPtr; // An atomic pointer to the Connection object so that you could do multi-thread activities on it
+    std::atomic<bool> _deactivateThread; //a switch for activating or deactivated the thread
     void receivePacketsThreadFunc();//Todo: create a thread each new callback
 
 public:
